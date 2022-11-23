876ae8ee6d9f4cf53ccc94d7a5166f0a024c7310
Tománek Filip <tomanek.fi.2022@skola.ssps.cz>
2022-11-23 08:05:43 +0100

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a number:");
+            string num = Console.ReadLine();
+            string flip = "";
+
+            for (int i = num.Length; i > 0; i--)
+            {
+                flip += num[i - 1];
+            }
+
+            bool res = false;
+
+            if (num == flip)
+            {
+                res = true;
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
